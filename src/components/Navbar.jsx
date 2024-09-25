4fdee205bf38d3f21dd1dfc771699ff787b0cfa0
Devansh Mayatra <devanshm667@gmail.com>
2024-09-25 19:32:40 +0530

    uodated routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
         <div className="siteName" >STUDENT NETWORK</div>
         <div className="options">OPTIONS</div>
         <ul className="navLinks">
-          <li><NavLink className={(e) => { return e.isActive ? "active" : "" }} to="/home">Home</NavLink></li>
+          <li><NavLink className={(e) => { return e.isActive ? "active" : "" }} to="/">Home</NavLink></li>
           <li><NavLink className={(e) => { return e.isActive ? "active" : "" }} to="/blog">Blog</NavLink></li>
           <li><NavLink className={(e) => { return e.isActive ? "active" : "" }} to="/file_share">File Share</NavLink></li>
           <li><NavLink className={(e) => { return e.isActive ? "active" : "" }} to="/members">Members</NavLink></li>
